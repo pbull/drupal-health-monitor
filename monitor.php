777b8c1ba48ef8934dbee9846e5f6aae548bb505
Peter Bull <github.com@purplepaste.com>
2012-08-09 11:10:03 -0300

    2012-08-09 @acquia-PB updating docs/comments

--- monitor.php
+++ monitor.php
@@ -2,8 +2,9 @@
 
 /**
  * Script to monitor health of a Drupal 7 site, useful for monitoring, load balancers, etc.
- * Takes an optional comma-separated string of options in the querystring:
- *   health.php?options=db,slavedb,memcache,files
+ * Takes an optional comma-separated string of options in the querystring to only run the
+ * requested checks. Default is to run all checks:
+ *   monitor.php?options=db,slavedb,memcache,files
  */
 
 /**
